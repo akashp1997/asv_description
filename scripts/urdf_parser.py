5c948afc3f86f88ec93ac906d1ea8f7eb009a691
Akash Purandare <akash.p1997@gmail.com>
2018-01-03 16:38:59 -0600

    Cleaned the package for better readability and understanding

--- scripts/urdf_parser.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import roslib
-roslib.load_manifest("urdfdom_py")
-import rospy
-
-from urdf_parser_py.urdf import URDF
-robot = URDF.from_parameter_server()
-print(robot.joints[0])
